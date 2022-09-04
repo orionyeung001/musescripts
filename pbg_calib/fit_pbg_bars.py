5e0c2b866cb094733bc896e070832ac741ce65b5
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2022-09-03 23:22:20 -0500

    clean and reimplement as more pure stats problem
    remove unneeded routines from `parse/process_rootfiles.py`
    added TODO notes to top of script
    loaded experiment data and set relation from bar position to barID
    tried to call minimizer for small set of bars
      no dice, might be fit or index issue

--- pbg_calib/fit_pbg_bars.py
+++ pbg_calib/fit_pbg_bars.py
@@ -1,99 +1,97 @@
-import os, sys, re
-import logging
+import re
+from sys import stderr
+from os import listdir
 import matplotlib.pyplot as plt
-import numpy as np, uproot, pandas as pd, scipy.optimize as optimize
+import numpy as np, awkward as ak
+import uproot, pandas as pd
+from scipy.optimize import minimize
 import itertools
+from scipy.stats import ks_2samp
 
-def compute_data(h):
-    """ given the raw hist, it outputs the efficiency ratios """
-    h_n = h.to_numpy()
-    return (h_n[0][1]/h_n[0][0])
+"""
+TODO:
+- [ ] use KDestiamtor to obtain callable for pdf of MC data, f_T(t)
+- [ ] write resampler of experiment data
+- [ ] write f_{X|T}(x;t;params) = normal.pdf(x | t+mu_p, (t*rel_sigma(t; params))^2 + sig_p^2)
+- [ ] write integrator with integrate.quad over t for
+      f_{X,T}(x,t;params) = f_{X|T}(x;t;params)*f_T(t) to get f_X(x;params)
+- [ ] write callable accepting params that evaluates komologorov test for
+      experiment came from f_X(x;params)
+- [ ] plot all them distributions (since T,X are in the same space)
+- [ ] run minimizer for a single bar, see what happens
+- [ ] run minimizer for all bars?
+"""
 
-def get_filenames():
-    data_path = '/Users/orion/muse/rootfiles/'
-    regex = re.compile(r'^run\d{5}_Trigger_timing\.root$')
-    return map(lambda fname: data_path + fname,
-            filter(regex.match, os.listdir(data_path))
-               )
 
-def append_values(out_dat, fname, f, obj_key):
-    run_num = int(fname.split('_')[0][-5:])
-    plot_title = f[obj_key].all_members['fTitle']
-    out_dat.loc[len(out_dat.index)] = [
-            run_num, # run number
-            get_beam_rate(run_num), # metadata
-            plot_title, # hist_identifier
-            compute_data(f[obj_key]), # data we want
-            ]
+def get_hists_from_cooker():
+    output_hists = dict()
+    fname = '/Users/orion/muse/rootfiles/run13057_PBG_Detail.root'
+    with uproot.open(fname) as f:
+        for x,y in itertools.product(range(8), repeat=2):
+            try:
+                obj_key = f'Individual Bars/x = {x}, y = {y}/QDC e Hits'
+                output_hists[(x,y)] = f[obj_key]
+            except KeyError:
+                print(f'could not find item {obj_key}', file=stderr)
+    return output_hists
 
-def find_matching_keys(pattern):
-    fnames = list(get_filenames())
-    with uproot.open(fnames[0]) as f:
-        obj_keys = f.keys(filter_name=pattern)
-        obj_keys = list(map(lambda s: s.split(';')[0], obj_keys))
-    return obj_keys
+def hist_distance(params, mc_photon, data_counts, data_edges):
+    a,b,c,Ecalib = params
+    E = mc_photon/Ecalib
 
-def create_matching_keys(*args):
-    """ returns '/'.join of each string as outer product from each list
-    Useful for traversing cooker output since each item has similar structure
-    Example use:
-    >> list(create_matching_keys(['a','b'],
-                                 ['fancy'],
-                                 ['x','yz']))
-        ['a/fancy/x', 'b/fancy/x', 'a/fancy/yz', 'b/fancy/yz']
-    """
-    return ["/".join(p) for p in itertools.product(*args)]
+    pedestal = np.random.normal(calib_pedestal, calib_pedestal_sigma,
+                                len(mc_photon))
 
-def process():
-    # init table of N x 4 with labeled columns, and how to add data to it
-    output_data = pd.DataFrame(columns=('run', 'rate', 'titlestr', 'ratio'))
+    # sample (E\pm\sigma_E) + pedestal noise
+    mc_qdc = np.random.normal(E, E*E_rel_sigma(params, mc_photon)) + pedestal
+    # account for qdc saturation
+    mc_qdc = np.clip(mc_qdc, 0, 3840)
+    mc_counts, _ = np.histogram(mc_qdc, data_edges)
+    return ks_2samp(data_counts, mc_counts).pvalue
 
-    # get the keys, preferred method is explicit formation of keys
-    obj_keys = create_matching_keys(['Ch16 BHD_PID_e', 'Ch17 BHD_PID_pi'],
-                                    ['raw timing'],
-                                    ('repeat multiplicity logic ' + e for e in ['fall','rise'])
-                                    )
-
-    # # can alternatively regex needs to be surrounded by /'s or shell glob match the keys
-    # obj_key_pattern = '/^Ch\d{2} BHD_PID_.*repeat multiplicity logic (rise|fall)/'
-    # obj_key_pattern = '*BHD_PID_*repeat multiplicity logic*'
-    # obj_keys = find_matching_keys(obj_key_pattern)
-
-    for fname in get_filenames():
-        abbr_fname = fname.split('/')[-1]
-        logging.debug(f'processing {abbr_fname}')
-
-        with uproot.open(fname) as f:
-            # iterate over matching hists in each matching file
-            for obj_key in obj_keys:
-                try:
-                    # .append_values() only has to know how to process one histogram
-                    # no need to modify anything in these nested for loops
-                    append_values(output_data, fname, f, obj_key)
-                except KeyError:
-                    logging.warning(f'could not find item {obj_key}')
-
-    return output_data
-
-def calib_lightoutput_rel_sigma(e, a=0.05, b=0.05, c=0.02, ):
-    return a / np.sqrt(e*0.001) + b + c / (e*0.001);
+def E_rel_sigma(params, mc_photon):
+    """ this will be provided to the optimizer to find the right values for
+    a,b,c = params """
+    a,b,c,Ecalib = params
+    E = y/Ecalib
+    return a / np.sqrt(E*0.001) + b + c / (E*0.001);
 
 def store_data():
-    g4_fname = '/Users/orion/muse/midasfiles/g4psi_13057.root'
-    ecalib = 2.4
-    with uproot.open(g4_fname) as f:
-        t = f['T']
-        df = t.arrays(['CAL_PbG_CopyID', 'CAL_PbG_PhotonY'], library='pd')
+    g4_fname = '/Users/orion/muse/midasfiles/g4psi_13057.root:T'
+    with uproot.open(g4_fname) as t:
+        arr = t.arrays(['CAL_PbG_CopyID', 'CAL_PbG_PhotonY'])
 
-    df['e'] = df.CAL_PbG_PhotonY.map(lambda y: y/ecalib)
-    df.to_hdf("store.h5", "df", append=True)
-    return None
+    # df.to_hdf("resources/store.h5", "df", append=True)
+    return arr
 
-# logging levels are CRITICAL, ERROR, WARNING, INFO, DEBUG, and NOTSET
-# we show that level and all higher priority levels
-logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL) # only shows critical messages
 
-df = pd.read_hdf("store.h5")
+calib_pedestal = 128.80
+calib_pedestal_sigma = 3.275
+bar_hists = get_hists_from_cooker()
+arr = store_data()
+n_hits = ak.num(arr['CAL_PbG_CopyID'])
+has_hits = arr[n_hits>0]
+
+print('a,b,c,Ecalib')
+for x,y in itertools.product(range(3), repeat=2):
+    # get cooker data
+    counts, edges = bar_hists[(x,y)].to_numpy()
+    edges = edges[1:]
+    counts = counts[np.logical_and(400<edges, edges<2000)]
+    edges = edges[np.logical_and(400<edges, edges<2000)]
+
+    # prep mc data
+    bar = x + 64 - ( (1+y) * 8 );
+    mc_photon = ak.flatten(has_hits[has_hits.CAL_PbG_CopyID == bar].CAL_PbG_PhotonY)
+    res = minimize(hist_distance, (0.05,0.05, 0.02, 2.4),
+           args = (mc_photon, counts, edges),
+           method = 'L-BFGS-B',
+           bounds = ((0.01, 0.1),(0.01, 0.1),(0.01, 0.04),(1,4)))
+    if res.success:
+        print(f'{res.x}')
+    else:
+        print(f'None,None,None,None')
+
 
 """ aim to find a,b,c,ecalib that maximizes likelihood that the data come
 from the same distributions """
